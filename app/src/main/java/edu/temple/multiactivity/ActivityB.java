29818829c241a7f97a2d40a876db2da37245452e
justinschmidt <justinschmidttt@gmail.com>
2018-09-12 09:33:45 -0400

    Made Button B work.

--- app/src/main/java/edu/temple/multiactivity/ActivityB.java
+++ app/src/main/java/edu/temple/multiactivity/ActivityB.java
@@ -1,17 +1,33 @@
 package edu.temple.multiactivity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivityB extends AppCompatActivity {
+
+    Button buttonB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        setTitle("Activity B");
-
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_b);
+        //Set Title
+        setTitle("Activity B");
+        //Get Button B
+        buttonB = findViewById(R.id.buttonB);
+        //Set up Button B
+        buttonB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startActivityIntent = new Intent(ActivityB.this, ActivityA.class);
+                startActivity(startActivityIntent);
+            }
+        });
+
+
     }
 }
